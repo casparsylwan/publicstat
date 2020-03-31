70fb02386191241b3a64e7e4fa5c3559346ff74b
casparsylwan <casparsylwan@gmail.com>
2020-03-31 22:59:49 +0200

    mediaqurery animations in ts frontpage.

--- src/app/components/main/frontsection/frontsection.component.ts
+++ src/app/components/main/frontsection/frontsection.component.ts
@@ -7,19 +7,258 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontsectionComponent implements OnInit {
 
-  //Booleans for section in focus
+  //State change new view
+  viewOld:number = 0;
+  views: number = 0;
 
-  openMainLeft:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public mainLeftFocus(){
+  public mainTopFocus() {}
 
-    this.openMainLeft = !this.openMainLeft;
+  public mainRigthTopFocus(){
+    console.log(this.views);
 
+    if(this.views===2){
+
+      this.views = 0;
+
+    }else{
+
+      this.views= 2;
+
+    }
+  }
+
+  public mainLeftFocus() {
+
+    if(this.views===1){
+
+      this.views = 0;
+
+    }else{
+
+      this.views= 1;
+
+    }
+  }
+
+  public mainRigthDown(){
+
+    if(this.views===3){
+
+      this.views = 0;
+
+    }else{
+
+      this.views= 3;
+
+    }
+
+  }
+
+ public stateChangeMainTop(){
+
+    switch(this.views){
+      case (1):{
+        return {
+          'height': '40rem',
+          'transition': 'height ease-in 500ms 500ms'
+        }
+        break;
+      }default : {
+        return {
+          'heigth' : '30rem',
+          'transition': 'height ease-in 500ms 500ms'
+        }
+      }
+    }
+
+  }
+
+  public stateChangeMainLeft() {
+
+    switch (this.views) {
+      case (1): {
+        console.log(this.views)
+        return {
+          'width': '80%'
+        }
+        break;
+      }
+      case (2): {
+        console.log(this.views)
+        return {
+          'width': '0%',
+          'transition': 'width ease-in 500ms 500ms'
+        }
+        break;
+      }
+      case (3): {
+        return{
+          'width': '0',
+          'transition': 'width ease-in 500ms'
+        }
+      }
+      default: {
+        console.log(this.views)
+        return {
+          'width': '50%',
+          'transition': 'width ease-in 500ms 0ms'
+        }
+        break;
+      }
+    }
+  }
+
+  public stateChangeMainRigth() {
+
+    switch (this.views) {
+      case (1): {
+        console.log(this.views)
+        return {
+          'width': '20%',
+          'heigth': '100%'
+        }
+        break;
+      }
+      case (2): {
+        console.log(this.views)
+        return {
+          'width': '50%',
+          'heigth': '150%'
+        }
+        break;
+      }
+      default: {
+        console.log(this.views)
+        return {
+          'width': '50%',
+          'heigth': '100%'
+        }
+        break;
+      }
+
+    }
+  }
+
+
+  public stateChangeMainRigthTop() {
+
+    switch (this.views) {
+      case (1): {
+        console.log(this.views)
+        return {
+          'width': '100%',
+          'height': '50%',
+          'transition': 'height ease-in 500ms 500ms'
+        }
+        break;
+      }
+      case (2): {
+        console.log(this.views)
+        return {
+          'width': '200%',
+          'height': '100%',
+          'transition': 'height ease-in 500ms 0ms, width ease-in 500ms 500ms'
+        }
+        break;
+      }case (3) :{
+        return {
+          'width': '200%',
+          'transition': 'width ease-in 500ms'
+        }
+      
+      
+      } default: {
+        console.log(this.views)
+        return {
+          'width': '100%',
+          'heigth': '50%',
+          'transition': 'height ease-in 500ms 500ms , width ease-in 500ms 0ms'
+        }
+        break;
+      }
+
+    }
+  }
+
+  public stateChangemaindown(){
+
+    switch(this.views){
+      case(1) : {
+        return { 'background-color': '#777700',
+                 'height' : '50%',
+                 'transition': 'height ease-in 500ms 0ms'
+                }
+      
+      }case(2) : {
+        return { 'background-color': '#777700',
+                 'height': '0',
+                 'transition': 'height ease-in 500ms 0ms'
+                }
+      
+              
+      }case (3) :{
+                return {
+                  'width': '200%',
+                  'transition': 'width ease-in 500ms'
+                }
+              
+              
+              }default : {
+        return { 'background-color': '#777700',
+                 'transition': 'height ease-in 500ms, width ease-in 500ms'
+                 
+                }
+      }
+    }
+  }
+
+  public stateChangeMainOptionField() {
+
+    switch (this.views) {
+      case (1): {
+        return {
+          'height': '0rem',
+          'border-bottom': '4px solid #fff',
+          'transition': 'all ease-in 500ms 0ms'
+        }
+        break;
+      } default: {
+        return {
+          'height': '4rem',
+          'border-bottom': '1px solid #fff',
+          'transition': 'all ease-in 300ms 700ms'
+          }
+          break;
+        }
+      }
+    }
+
+
+  public viewCange() {
+
+    switch (this.views) {
+      case (1): {
+        console.log(this.views)
+        return '#fff'
+        break;
+      }
+      case (2): {
+        console.log(this.views)
+        return '#ff0'
+        break;
+      }
+      default: {
+        console.log(this.views)
+        return '#00f'
+        break;
+      }
+    }
   }
 
 }
